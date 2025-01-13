0cdf5686f5e28df3b1cb77a680ca8f083fb58f86
Cole Khamnei <cole.khamnei@gmail.com>
2025-01-13 18:15:10 -0500

    rte

--- src/matrix.py
+++ src/matrix.py
@@ -5,8 +5,16 @@ from tqdm.auto import tqdm
 
 def to_np(array):
     """ """
-    if isinstance(array, torch.tensor):
+    if isinstance(array, torch.Tensor):
         return array.cpu().numpy()
+    return array
+
+
+def to_torch(array, **devp):
+    """ """
+    if not isinstance(array, torch.Tensor):
+        return torch.tensor(array.astype("float32"), **devp)
+
     return array
 
 
@@ -39,12 +47,72 @@ class ev_aggregator:
         return to_np(self.ev_maxs)
 
 
-def block_analysis(data, aggregator_class, block_size=1000, use_torch=True, device="mps"):
+class sparse_aggregator:
+    """
+    keeps running list of coordinates of top p percent (or top n) sparse connections:
+
+    """
+    def __init__(self, data, device, use_torch=True, sparsity_percent=0.1):
+        self.size = data.shape[1]
+        self.device = device
+        self.devp = {"device": device} if use_torch else {}
+        self.use_torch = use_torch
+
+        self.sparsity_frac = sparsity_percent / 100
+        self.top_n = int(np.ceil(self.size ** 2 * self.sparsity_frac))
+        print(self.top_n)
+
+        if use_torch:
+            self.top_k = lambda values, k, axis: torch.topk(values, k, dim=axis)
+            self.backend = torch
+        else:
+            self.backend = np
+            self.top_k = lambda values, k, axis: np.partition(values, -k, axis=axis)[-k:]
+
+        self.top_values = -self.backend.ones(self.top_n, **self.devp)
+        self.top_ci = -self.backend.ones(self.top_n, **self.devp)
+        self.top_ri = -self.backend.ones(self.top_n, **self.devp)
+
+    def __call__(self, A, B, a_index, b_index):
+        """ """
+        backend = self.backend
+        M_chunk = backend.corrcoef(backend.hstack([A, B]).T)[:A.shape[1], A.shape[1]:] ** 2
+        if a_index == b_index:
+            M_chunk[~backend.eye(M_chunk.shape[0], dtype=bool)] = -10
+            # M_chunk = M_chunk[~backend.eye(M_chunk.shape[0], dtype=bool)].reshape(A.shape[1], -1)
+            # raise NotImplementedError
+
+        M_chunk_flat = M_chunk.flatten()
+
+        adjusted_k = min(self.top_n, to_np(M_chunk.shape[0] * M_chunk.shape[1]))
+        # Get top n values and their indices
+        chunk_tv, chunk_ti = self.top_k(M_chunk_flat, adjusted_k, axis=0)
+
+        # Convert the flat indices back to row and column indices
+        chunk_ri, chunk_ci = backend.unravel_index(chunk_ti, M_chunk.shape)
+
+        chunk_ri = backend.arange(a_index.start, a_index.stop, **self.devp)[chunk_ri]
+        chunk_ci = backend.arange(b_index.start, b_index.stop, **self.devp)[chunk_ci]
+
+        compare_ri = backend.hstack([chunk_ri, self.top_ri])
+        compare_ci = backend.hstack([chunk_ci, self.top_ci])
+        compare_tv = backend.hstack([chunk_tv, self.top_values])
+
+        self.top_values, update_ti = self.top_k(compare_tv, self.top_n, axis=0) # TODO: need axis?
+        self.top_ci[update_ti] = compare_ci[update_ti]
+        self.top_ri[update_ti] = compare_ri[update_ti]
+
+    def results(self):
+        return self.top_values, self.top_ri, self.top_ci
+        return to_np(self.top_values)
+
+
+def block_analysis(data, aggregator_class, block_size=1000, use_torch=True, device="mps", **agg_params):
     """ """
+
     if use_torch:
-        data = torch.tensor(data.astype("float32"), device=device)
-    
-    aggregator = aggregator_class(data, device=device, use_torch=use_torch)
+        data = to_torch(data, device=device)
+    aggregator = aggregator_class(data, device=device, use_torch=use_torch, **agg_params)
 
     n_blocks = int(np.ceil(data.shape[1] / block_size))
     overhang_size = data.shape[1] % (n_blocks - 1) + 1
@@ -54,7 +122,7 @@ def block_analysis(data, aggregator_class, block_size=1000, use_torch=True, devi
         a_index = slice(a_start, a_start + a_n_items)
         a_block = data[:, a_index]
     
-        for b_count in tqdm(range(n_blocks), leave=False):
+        for b_count in range(n_blocks):
             b_start = b_count * block_size
             b_n_items = block_size if b_count < n_blocks - 1 else overhang_size
             b_index = slice(b_start, b_start + b_n_items)
