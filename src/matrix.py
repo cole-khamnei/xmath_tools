d9de9d302106808e997941bcb04f4c796017bb1e
Cole Khamnei <cole.khamnei@gmail.com>
2025-01-24 11:18:18 -0500

    rte

--- src/matrix.py
+++ src/matrix.py
@@ -42,16 +42,26 @@ def get_device(device_str=None):
     return 'cpu'
 
 
-def torch_corr(a, b):
+def torch_corr(x, y):
     """ """
-    a, b = a.T, b.T
-    return torch.matmul(a, b.T) / (torch.norm(a, dim=1, keepdim=True) * torch.norm(b, dim=1))
+    x, y = x.T, y.T
+    x_demeaned = x - x.mean(axis=1, keepdims=True)
+    y_demeaned = y - y.mean(axis=1, keepdims=True)
+
+    x_norm = x_demeaned / torch.sqrt(torch.sum(x_demeaned ** 2, axis=1, keepdims=True))
+    y_norm = y_demeaned / torch.sqrt(torch.sum(y_demeaned ** 2, axis=1, keepdims=True))
+    return x_norm @ y_norm.T
 
 
-def np_corr(a, b):
+def np_corr(x, y):
     """ """
-    a, b = a.T, b.T
-    return np.matmul(a, b.T) / (np.linalg.norm(a, axis=1, keepdims=True) * np.linalg.norm(b, axis=1))
+    x, y = x.T, y.T
+    x_demeaned = x - x.mean(axis=1, keepdims=True)
+    y_demeaned = y - y.mean(axis=1, keepdims=True)
+
+    x_norm = x_demeaned / np.sqrt(np.sum(x_demeaned ** 2, axis=1, keepdims=True))
+    y_norm = y_demeaned / np.sqrt(np.sum(y_demeaned ** 2, axis=1, keepdims=True))
+    return x_norm @ y_norm.T
 
 
 # ----------------------------------------------------------------------------# 
@@ -67,7 +77,7 @@ def get_nnz_safe(array):
     return np.count_nonzero(array)
 
 
-def to_array(arr):
+def sparse_to_array(arr):
     """ """
     return arr.toarray() if scipy.sparse.issparse(arr) else arr
 
@@ -127,6 +137,7 @@ class BlockAnalysis:
         self.symmetric=symmetric
 
         self.backend = torch if use_torch else np
+        self.create_empty = lambda k=1: [self.backend.ones(0, **self.devp) for _ in range(k)]
 
     def core_func(self, A, B, a_index, b_index):
         """ """
@@ -135,6 +146,29 @@ class BlockAnalysis:
     def __call__(self, A, B, a_index, b_index, mask=None, exclude_index=None):
         """ """
         raise NotImplementedError
+
+    def get_mask_chunk_index(self, mask, a_index, b_index, select_index):
+        """ """
+        if mask is not None:
+            mask_chunk = mask[a_index, b_index]
+            if get_nnz_safe(mask_chunk) > 0:
+                mask_flat = ~sparse_to_array(mask_chunk.astype(bool)).ravel()
+                # TODO: move to_torch to a to_backend_array() which creates either np or torch
+                mask_flat = to_torch(mask_flat, dtype=bool, **self.devp)
+                select_index &= mask_flat
+
+        return select_index
+
+    def get_exclude_chunk_index(self, exclude_index, a_index, b_index, select_index):
+        """ """
+        if exclude_index is not None:
+            a_exclude = exclude_index[a_index]
+            b_exclude = exclude_index[b_index]
+
+            exclude_chunk = a_exclude.reshape(-1, 1) @ b_exclude.reshape(1, -1)
+            select_index &= (exclude_chunk == 0).flatten()
+
+        return select_index
 
     @classmethod
     def run(cls, data, mask=None, exclude_index=None,
@@ -153,6 +187,11 @@ class BlockAnalysis:
             if exclude_index is not None:
                 exclude_index = to_torch(exclude_index, device=device, dtype="float16")
 
+        if symmetric and mask is not None:
+            if get_nnz_safe(mask != mask.T) == 0:
+                print("Mask is not symmetric, cannot use symmetric acceleration.")
+                symmetric = False
+
         aggregator = cls(data, device=device, use_torch=use_torch, symmetric=symmetric, **agg_params)
 
         n_blocks = int(np.ceil(data.shape[1] / block_size))
@@ -166,7 +205,7 @@ class BlockAnalysis:
             a_index = slice(a_start, a_start + a_n_items)
             a_block = data[:, a_index]
 
-            b_count_iter = range(a_count) if symmetric else range(n_blocks)
+            b_count_iter = range(a_count + 1) if symmetric else range(n_blocks)
 
             for b_count in b_count_iter:
                 b_start = b_count * block_size
@@ -196,10 +235,13 @@ class SparseAggregator(BlockAnalysis):
 
         self.sparsity_frac = sparsity_percent / 100
 
+
+        # TODO: determine if sparsity percent should be before or after diagonal skip
+        n_items = self.size ** 2 - self.size if self.skip_diagonal else self.size ** 2
         if self.symmetric:
-            self.top_n = int(np.ceil(self.size ** 2 * self.sparsity_frac)) // 2
+            self.top_n = int(np.ceil(n_items * self.sparsity_frac)) // 2
         else:
-            self.top_n = int(np.ceil(self.size ** 2 * self.sparsity_frac))
+            self.top_n = int(np.ceil(n_items * self.sparsity_frac))
 
         if kwargs["use_torch"]:
             self.top_k = lambda values, k, axis=0: torch.topk(values, k, dim=axis)
@@ -212,23 +254,20 @@ class SparseAggregator(BlockAnalysis):
         self.min_tv = -self.backend.inf
         self.cache_tv, self.cache_ri, self.cache_ci = [], [], []
         self.compare_tv, self.compare_ri, self.compare_ci = self.create_empty(3)
-        self.diag_symmetry_index = self.backend.torch.triu_indices(self.size, self.size, offset=1)
 
-    def compare_cache(self):
+    def compare_cache(self, final=False):
         """ """
         backend = self.backend
         self.compare_tv = backend.hstack(self.cache_tv + [self.compare_tv])
         self.compare_ri = backend.hstack(self.cache_ri + [self.compare_ri])
         self.compare_ci = backend.hstack(self.cache_ci + [self.compare_ci])
+        self.cache_tv, self.cache_ri, self.cache_ci = [], [], []
 
         if len(self.compare_tv) > self.top_n:
-            # print("compare")
             self.compare_tv, update_ti = self.top_k(self.compare_tv, self.top_n)
             self.compare_ri = self.compare_ri[update_ti]
             self.compare_ci = self.compare_ci[update_ti]
-
             self.min_tv = self.compare_tv[-1]
-            self.cache_tv, self.cache_ri, self.cache_ci = [], [], []
 
     def __call__(self, A, B, a_index, b_index, mask=None, exclude_index=None):
         """ """
@@ -244,9 +283,8 @@ class SparseAggregator(BlockAnalysis):
 
         if mask is not None:
             mask_chunk = mask[a_index, b_index]
-
             if get_nnz_safe(mask_chunk) > 0:
-                mask_flat = ~to_array(mask_chunk.astype(bool)).ravel()
+                mask_flat = ~sparse_to_array(mask_chunk.astype(bool)).ravel()
                 mask_flat = to_torch(mask_flat, dtype=bool, **self.devp)
                 threshold_chunk_tv_index &= mask_flat
 
@@ -255,14 +293,16 @@ class SparseAggregator(BlockAnalysis):
 
         M_chunk = self.core_func(A, B, a_index, b_index)
 
+        # TODO: Fix symmetry:
         if self.symmetric and a_index == b_index:
-            threshold_chunk_tv_index &= self.diag_symmetry_index
+            triu_index = backend.triu_indices(*M_chunk.shape, offset=1)
+            flat_triu = triu_index[0] * M_chunk.shape[0] + triu_index[1]
+            threshold_chunk_tv_index[flat_triu] = 0
 
         chunk_tv = M_chunk.flatten()
         threshold_chunk_tv_index &= chunk_tv > self.min_tv
 
         chunk_ti = backend.where(threshold_chunk_tv_index)[0]
-
         if len(chunk_ti) == 0:
             return
 
@@ -279,15 +319,17 @@ class SparseAggregator(BlockAnalysis):
             self.compare_cache()
 
     def results(self):
-        self.compare_cache()
+        self.compare_cache(final=True)
 
         assert len(self.cache_tv) == 0
-        assert len(self.compare_tv) == self.top_n
+        # assert len(self.compare_tv) == self.top_n
 
         tv, ri, ci = to_np((self.compare_tv, self.compare_ri, self.compare_ci))
-
         if self.symmetric:
-            tv, ri, ci = np.hstack([tv, tv]), np.hstack([ri, ci]), np.hstack([ci, ri])
+            non_diag_index = ri != ci
+            tv = np.hstack([tv, tv[non_diag_index]])
+            # following is purposely done in one line
+            ri, ci = np.hstack([ri, ci[non_diag_index]]), np.hstack([ci, ri[non_diag_index]])
 
         return scipy.sparse.csr_matrix((tv, (ri.astype(int), ci.astype(int))), shape=self.shape)
 
@@ -329,6 +371,84 @@ class Runner(BlockAnalysis):
 
     def results(self):
         return None
+
+#\section threshold aggregator
+
+class ThresholdAggregator(BlockAnalysis):
+    """
+    keeps running list of coordinates of top p percent (or top n) sparse connections:
+
+    """
+    def __init__(self, *args, threshold=0.1, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.threshold = threshold
+        self.cache_tv, self.cache_ri, self.cache_ci = [], [], []
+
+    def __call__(self, A, B, a_index, b_index, mask=None, exclude_index=None):
+        """ """
+        backend = self.backend
+
+        # TODO: move preselection select index generation to blockanalysis method
+        select_index = backend.ones(A.shape[1] * B.shape[1], dtype=bool, **self.devp)
+        select_index = self.get_mask_chunk_index(mask, a_index, b_index, select_index)
+        select_index = self.get_exclude_chunk_index(exclude_index, a_index, b_index, select_index)
+
+        if not select_index.any():
+            return
+
+        M_chunk = self.core_func(A, B, a_index, b_index)
+
+        # TODO: Fix symmetry:
+        if self.symmetric and a_index == b_index:
+            triu_index = backend.triu_indices(*M_chunk.shape, offset=1)
+            flat_triu = triu_index[0] * M_chunk.shape[0] + triu_index[1]
+            select_index[flat_triu] = 0
+
+        chunk_tv = M_chunk.flatten()
+        select_index &= chunk_tv > self.threshold
+
+        chunk_ti = backend.where(select_index)[0]
+        if len(chunk_ti) == 0:
+            return
+
+        chunk_tv = chunk_tv[chunk_ti]
+        chunk_ri, chunk_ci = backend.unravel_index(chunk_ti, M_chunk.shape)
+
+        self.cache_tv.append(chunk_tv)
+        self.cache_ri.append(chunk_ri + a_index.start)  # off set row index by a start #TODO: check if this is right
+        self.cache_ci.append(chunk_ci + b_index.start)  # off set col index by b start
+
+    def results(self):
+
+        self.cache_tv = self.backend.hstack(self.cache_tv)
+        self.cache_ri = self.backend.hstack(self.cache_ri)
+        self.cache_ci = self.backend.hstack(self.cache_ci)
+
+        tv, ri, ci = to_np((self.cache_tv, self.cache_ri, self.cache_ci))
+        if self.symmetric:
+            non_diag_index = ri != ci
+            tv = np.hstack([tv, tv[non_diag_index]])
+            # following is purposely done in one line
+            ri, ci = np.hstack([ri, ci[non_diag_index]]), np.hstack([ci, ri[non_diag_index]])
+
+        return scipy.sparse.csr_matrix((tv, (ri.astype(int), ci.astype(int))), shape=self.shape)
+
+
+class ThresholdCorrelator(ThresholdAggregator):
+    """ """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.corr_func = torch_corr if kwargs["use_torch"] else np_corr
+
+    def core_func(self, A, B, a_index, b_index):
+        """ """
+        backend = self.backend
+        M_chunk = self.corr_func(A, B)
+        if a_index == b_index and self.skip_diagonal:
+            M_chunk[backend.eye(M_chunk.shape[0], dtype=bool)] = -backend.inf
+
+        return M_chunk
 
 
 # ----------------------------------------------------------------------------# 
