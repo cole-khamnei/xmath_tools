f8418abc214a7ef4109e7ac16b89fc91544cc83d
Cole Khamnei <cole.khamnei@gmail.com>
2025-02-18 17:45:10 -0500

    working runners, now to do aggregators

--- src/correlators.py
+++ src/correlators.py
@@ -68,45 +68,16 @@ class Maxxer(Correlator, block_analysis.BlockAnalysis):
 # ----------------------------------------------------------------------------# 
 
 
-class SparseCorrelator(aggregators.SparseAggregator):
+class SparseCorrelator(Correlator, aggregators.SparseAggregator):
     """ """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.corr_func = utils.backend_corr
-
-    def core_func(self, A, B, a_index, b_index):
-        """ """
-        M_chunk = self.corr_func(self.backend, A, B)
-        if a_index == b_index and self.skip_diagonal:
-            M_chunk[self.backend.eye(M_chunk.shape[0], dtype=bool)] = -self.backend.inf
-            # M_chunk[self.backend.eye(M_chunk.shape[0], dtype=bool)] = float("nan")
-
-        return M_chunk
 
 
-class ThresholdCorrelator(aggregators.ThresholdAggregator):
+class ThresholdCorrelator(Correlator, aggregators.ThresholdAggregator):
     """ """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.corr_func = utils.backend_corr
-
-    def preprocess(self, data):
-        """ norm data so correlation is just multiplication """
-        data = utils.backend_norm(backend, data)
-        self.preprocessed = False
-        return data
-
-    def core_func(self, A, B, a_index, b_index):
-        """ """
-        if self.preprocessed:
-            M_chunk = A.T @ B
-        else:
-            M_chunk = self.corr_func(self.backend, A, B)
-
-        if a_index == b_index and self.skip_diagonal:
-            M_chunk[self.backend.eye(M_chunk.shape[0], dtype=bool)] = -self.backend.inf
-
-        return M_chunk
 
 
 # ----------------------------------------------------------------------------# 
